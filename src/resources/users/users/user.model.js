5b21ad135b57d65f496bb9f9a297f0dd5084e22a
hlusovich <bigklu95@gmail.com>
2021-12-05 14:03:48 +0300

    add entry point  for Hape js

---
+++ src/resources/users/users/user.model.js
@@ -0,0 +1,17 @@
+const uuid = require('uuid');
+
+class User {
+  constructor({ name, login, password, id }) {
+    this.id = id || uuid.v4();
+    this.name = name;
+    this.login = login;
+    this.password = password;
+  }
+
+  static toResponse(user) {
+    const { id, name, login } = user;
+    return { id, name, login };
+  }
+}
+
+module.exports = User;
